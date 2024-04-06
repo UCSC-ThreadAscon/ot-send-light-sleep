f53a570bbf3501a7c5e366e305ef75d1420d1a9f
Simeon Tran <siatran@ucsc.edu>
2024-04-06 14:28:22 -0700

    Don't send any packets right now.

--- main/esp_ot_sleepy_device.c
+++ main/esp_ot_sleepy_device.c
@@ -233,10 +233,5 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_openthread_sleep_device_init());
     xTaskCreate(ot_task_worker, "ot_power_save_main", 4096, NULL, 5, NULL);
 
-    otSockAddr aSockName;
-    otUdpSocket aSocket;
-    udpSendInfinite(esp_openthread_get_instance(),
-                    UDP_SOCK_PORT, UDP_DEST_PORT,
-                    &aSockName, &aSocket);
     return;
 }
